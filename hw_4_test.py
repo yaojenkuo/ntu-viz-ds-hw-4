d84af752b5cbcc295b2c0abf7264b2f82dccde4c
yaojenkuo <tonykuoyj@gmail.com>
2020-05-28 12:42:54 +0800

    first commit

---
+++ hw_4_test.py
@@ -0,0 +1,50 @@
+import pandas as pd
+from pandas.testing import assert_frame_equal
+
+def generate_test_dict():
+    print("Fetching test data...")
+    test_dict = dict()
+    for i in range(1, 6):
+        dict_k = "question_{}".format(i)
+        data_name = "test_df_{}.feather".format(i)
+        dict_v = pd.read_feather(data_name)
+        test_dict[dict_k] = dict_v
+    return test_dict
+
+def generate_answer_dict(answer_list):
+    print("Fetching our answers...")
+    answer_dict = dict()
+    for i, answer in enumerate(answer_list):
+        dict_k = "question_{}".format(i + 1)
+        dict_v = answer_list[i]
+        answer_dict[dict_k] = dict_v
+    return answer_dict
+
+def run_tests(test_dict, answer_dict):
+    summary = {
+        'Question 1': 0,
+        'Question 2': 0,
+        'Question 3': 0,
+        'Question 4': 0,
+        'Question 5': 0
+    }
+    print("###### TESTING STARTS ######")
+    for i in range(1, 6):
+        dict_key = "question_{}".format(i)
+        answer_df = answer_dict[dict_key]
+        test_df = test_dict[dict_key]
+        try:
+            answer_df_str = answer_df.astype(str)
+            test_df_str = test_df.astype(str)
+            answer_df.columns = test_df.columns
+            answer_df.index = test_df.index
+            assert_frame_equal(test_df, answer_df)
+            summary["Question {}".format(i)] = 2
+            print("Congrats! answer {} passed!".format(i))
+        except:
+            print("Unfortunately, answer {} did not pass, try again!".format(i))
+    print("###### TESTING ENDS ######")
+    print("Submission Summary")
+    for k, v in summary.items():
+        print("{}: {}".format(k, v))
+    print("Total Points on Homework 4: {}".format(sum(summary.values())))
